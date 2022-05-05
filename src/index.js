f7c6e144fac34a0a9d2f6c24999edb6527885585
Francisca Villavicencio <fran.villavicencioa@gmail.com>
2022-05-05 00:52:53 -0400

    condicionales y arreglos en JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//react Dom tiene el metodo render, el que recibe dos parametros: 1- lo que queremos mostrar 2- donde lo vamos a mostrar.
+
+// lo que creamos aqui es una plantilla, que luego insertamos en el DOM de nuestra pagina. 
+const nombre = 'Fran'
+const sesion = true;
+const pais = 'Chile';
+const amigos = ['Gery', 'Nini', 'Pancho'];
+
+const JSX = (
+//OTRA FORMA DE UTILIZAR CONDICIONALES, ES CON UN OPERADOR TERNARIO. QUE EN ESTE CASO SE AGREGA DENTRO DE NUESTRO CODIGO JSX
+<>
+{sesion === true ? 
+<> 
+<h1 className= "titulo"> Hola {nombre}!</h1>
+<p>al fin es viernes</p>
+{pais && <p>Tu eres de {pais} </p>}
+</> 
+: <h1>No haz iniciado sesion</h1>}
+<ul>
+    {amigos.map((amigo, index) => <li key = {index}> {amigo}</li>
+    )}
+</ul>
+</> 
+);
+
+/* 
+// ESTA ES UNA FORMA DE UTILIZAR CONDICIONALES
+const verificarSesion = (sesion) => {
+if(sesion === true){
+    return JSX;
+}else{
+    return <h1>No has iniciado sesion</h1>
+}
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ verificarSesion(sesion)
+); */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ JSX
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
